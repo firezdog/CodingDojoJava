d17f44d40651bfb157c117eadc48e52cd9872625
Alex <firezdog@yahoo.com>
2018-07-04 23:14:30 -0500

    added first basic assignments

---
+++ Fundamentals/AboutMe/AboutMe.java
@@ -0,0 +1,7 @@
+public class AboutMe {
+    public static void main(String[] args){
+        System.out.println("My name is Alex Leibowitz.");
+        System.out.println("I was born in 1983.");
+        System.out.println("I am from Denver, CO.");
+    }
+}
